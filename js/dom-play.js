c4230cfc1e52a6c7503cc2c20dcf9df510f4e8c9
Kiki Wu <kiki.wu03@icloud.com>
2023-12-13 07:26:02 -0800

    started assignment

---
+++ js/dom-play.js
@@ -0,0 +1,14 @@
+//alert("Hello from dom play");
+
+//document.querySelector(".hamlet").style.backgroundColor = "orange";
+
+function highlight(el){
+
+    if(el.style.backgroundColor== 'blue'){
+        el.style.backgroundColor = 'pink'
+    }else{
+        el.style.backgroundColor = 'blue'
+    }
+
+
+}
